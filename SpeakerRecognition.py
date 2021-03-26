c65f582cd5a630e16dc7def3c078b61f94998170
cshaul <32314502+cshaul@users.noreply.github.com>
2021-03-26 14:18:02 -0600

    Original commit of the SpeakerRecognition.py file.

---
+++ SpeakerRecognition.py
@@ -0,0 +1,39 @@
+# ENCM509 
+# Group 3 Term Project : Speaker Recognition Using Python
+# Group Members: Charnjeet Gill, Nick Park, Carter Shaul
+
+
+# %% Read in training and testing data and create two data sets
+
+# TODO divide the data into a training and testing set. Use 1 or 2 subjects for validation
+
+#-------------------------------------- END SECTION --------------------------------------#
+
+# %% Perform feature extraction on the data
+
+# TODO For each recording (training and test) perform the necessary feature extraction
+
+
+#-------------------------------------- END SECTION --------------------------------------#
+
+# %% Train the GMM using the training samples
+
+#-------------------------------------- END SECTION --------------------------------------#
+
+# %% Perform classification using the testing data
+
+# TODO Record all the classification scores and generate a confusion matrix
+
+#-------------------------------------- END SECTION --------------------------------------#
+
+# %% Perform validation on a probe sample (not in the database) and match it against the trained model
+
+# TODO Record the classification score
+
+#-------------------------------------- END SECTION --------------------------------------#
+
+# %% Evaluation
+
+# TODO Evaluate the error rates using the confusion matrix
+
+#-------------------------------------- END SECTION --------------------------------------#
