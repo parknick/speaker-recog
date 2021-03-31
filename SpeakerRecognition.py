83e62ad848b0b81906b6c6098f48eb7a300e2dda
cshaul <32314502+cshaul@users.noreply.github.com>
2021-03-31 12:11:19 -0600

    Added Gaussian Mixture based scoring for features

--- SpeakerRecognition.py
+++ SpeakerRecognition.py
@@ -6,6 +6,7 @@ import python_speech_features as speech
 
 from sklearn.mixture import GaussianMixture
 
+
 # %% Read in training and testing data and create two data sets
 
 NUM_TRAINING_SAMPLES = 3
@@ -19,6 +20,8 @@ for i in range(1, NUM_TRAINING_SAMPLES + 1):
     training_set.append(wav.read('./wav-files/0%d_train.wav' % i))
     testing_set.append(wav.read('./wav-files/0%d_test.wav' % i))
 
+for i in range(training_set[0][1].size):
+    print(training_set[0][1][i])
 #-------------------------------------- END SECTION --------------------------------------#
 
 # %% Perform feature extraction on the data
@@ -31,14 +34,44 @@ for i in range(NUM_TRAINING_SAMPLES):
 
 #-------------------------------------- END SECTION --------------------------------------#
 
-# %% Train the GMM using the training samples
+# %% Train the GMM models using the training samples
+NUM_G_COMPONENTS = 10
+
+gmm = []
+for i in range(NUM_TRAINING_SAMPLES):
+    gmm.append(GaussianMixture(n_components=NUM_G_COMPONENTS).fit(training_features[i]))
 
 #-------------------------------------- END SECTION --------------------------------------#
 
 # %% Perform classification using the testing data
+c_mat = np.zeros((3,3))
 
+test1_scores = []
+for i in range(len(gmm)):
+    test1_scores.append(gmm[i].score(testing_features[0]))
+
+test2_scores = []
+for i in range(len(gmm)):
+    test2_scores.append(gmm[i].score(testing_features[1]))
+
+test3_scores = []
+for i in range(len(gmm)):
+    test3_scores.append(gmm[i].score(testing_features[2]))
+
+c_mat[0][0] = np.mean(test1_scores[0])
+c_mat[1][0] = np.mean(test2_scores[0])
+c_mat[2][0] = np.mean(test3_scores[0])
+
+c_mat[0][1] = np.mean(test1_scores[1])
+c_mat[1][1] = np.mean(test2_scores[1])
+c_mat[2][1] = np.mean(test3_scores[1])
+
+c_mat[0][2] = np.mean(test1_scores[2])
+c_mat[1][2] = np.mean(test2_scores[2])
+c_mat[2][2] = np.mean(test3_scores[2])
+
+print(c_mat)
 # TODO Record all the classification scores and generate a confusion matrix
-
 #-------------------------------------- END SECTION --------------------------------------#
 
 # %% Perform validation on a probe sample (not in the database) and match it against the trained model
