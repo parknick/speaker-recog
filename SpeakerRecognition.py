8504461c2d06ec603d231fd9fd3485cb05b054e6
cshaul <32314502+cshaul@users.noreply.github.com>
2021-04-07 18:22:53 -0600

    Working version. Ready for addition of samples

--- SpeakerRecognition.py
+++ SpeakerRecognition.py
@@ -10,8 +10,8 @@ from utils import plot_confusion_matrix
 
 # %% High level training parameters
 
-NUM_SUBJECTS = 2 # Change these values when you add or remove training and testing samples
-NUM_TRAINING_SAMPLES_PER_SUBJECT = 1 
+NUM_SUBJECTS = 4 # Change these values when you add or remove training and testing samples
+NUM_TRAINING_SAMPLES_PER_SUBJECT = 3 
 NUM_TESTING_SAMPLES_PER_SUBJECT = 2
 
 NUM_G_COMPONENTS = 10 # Number of gaussian mixture components
@@ -28,9 +28,7 @@ testing_labels = []
 #   Sample freq
 #   Concatenated wav data from each training file for the subject
 for i in range(1, NUM_SUBJECTS + 1):
-
     training_set.append(list(wav.read('./wav-files/subject_%d_train_1.wav' % i)))
-    print('./wav-files/subject_%d_train_1.wav' % i)
     for j in range(2, NUM_TRAINING_SAMPLES_PER_SUBJECT + 1):
         training_sample = list(wav.read('./wav-files/subject_%d_train_%d.wav' % (i, j)))
         training_set[i-1][1] = np.concatenate((training_set[i-1][1], training_sample[1]), axis=0)
@@ -38,14 +36,13 @@ for i in range(1, NUM_SUBJECTS + 1):
 # Read in all of the test wav files into the test set
 # Each entry in the test set will be a tuple with the folowing elements
 #   Sample freq
-#   Concatenated wav data from each test file for the subject
+#   Wav data from each test file for the subject
 for i in range(1, NUM_SUBJECTS + 1):
-    testing_set.append(list(wav.read('./wav-files/subject_%d_test_1.wav' % i)))
-    testing_labels.append(i)
-    for j in range(2, NUM_TESTING_SAMPLES_PER_SUBJECT + 1):
+    for j in range(1, NUM_TESTING_SAMPLES_PER_SUBJECT + 1):
         testing_sample = list(wav.read('./wav-files/subject_%d_test_%d.wav' % (i, j)))
-        testing_set[i-1][1] = np.concatenate((testing_set[i-1][1], testing_sample[1]), axis=0)
+        testing_set.append(testing_sample)
         testing_labels.append(i)
+
 
 #-------------------------------------- END SECTION --------------------------------------#
 
@@ -56,10 +53,10 @@ testing_features = []
 
 # Extracting the melcesptrum coefficients for each subjects set of training and testing data
 for i in range(NUM_SUBJECTS):
-    training_features.append(speech.mfcc(training_set[i][1], training_set[i][0]))
+    training_features.append(speech.mfcc(training_set[i][1], samplerate=8000))
 
-for i in range(NUM_SUBJECTS):
-    testing_features.append(speech.mfcc(testing_set[i][1], testing_set[i][0]))
+for i in range(NUM_TESTING_SAMPLES_PER_SUBJECT * NUM_SUBJECTS):
+    testing_features.append(speech.mfcc(testing_set[i][1], samplerate=8000))
 
 #-------------------------------------- END SECTION --------------------------------------#
 
@@ -76,31 +73,15 @@ for i in range(NUM_SUBJECTS):
 # %% Perform classification using the testing data
 predicted_labels = []
 
-test1_scores = []
-for i in range(len(gmm)):
-    test1_scores.append(gmm[i].score(testing_features[0]))
-predicted_labels.append(test1_scores.index(max(test1_scores)) + 1)
+print(len(testing_features))
 
-test2_scores = []
-for i in range(len(gmm)):
-    test2_scores.append(gmm[i].score(testing_features[1]))
-predicted_labels.append(test2_scores.index(max(test2_scores)) + 1)
-
-""" test3_scores = []
-for i in range(len(gmm)):
-    test3_scores.append(gmm[i].score(testing_features[2]))
-predicted_labels.append(test3_scores.index(max(test3_scores)) + 1)
-
-test4_scores = []
-for i in range(len(gmm)):
-    test4_scores.append(gmm[i].score(testing_features[3]))
-predicted_labels.append(test4_scores.index(max(test4_scores)) + 1)
-
-test5_scores = []
-for i in range(len(gmm)):
-    test5_scores.append(gmm[i].score(testing_features[4]))
-predicted_labels.append(test5_scores.index(max(test5_scores)) + 1) """
-
+scores = []
+for i in range(len(testing_features)):
+    for j in range(len(gmm)):
+        scores.append(gmm[j].score(testing_features[i]))
+    
+    predicted_labels.append(scores.index(max(scores)) + 1)
+    scores.clear()
 
 confusionMatrix = confusion_matrix(testing_labels, predicted_labels)
 plot_confusion_matrix(cm=confusionMatrix, target_names = [i for i in range(1, NUM_TESTING_SAMPLES_PER_SUBJECT+1)])
